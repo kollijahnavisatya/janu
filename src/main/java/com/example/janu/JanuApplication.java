5f0db85e4900bbf67d9e2cf101e86487e3371437
unknown <sathisusmitha@gmail.com>
2019-06-22 09:59:46 +0530

    first commit

---
+++ src/main/java/com/example/janu/JanuApplication.java
@@ -0,0 +1,13 @@
+package com.example.janu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanuApplication.class, args);
+	}
+
+}
